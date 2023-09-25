837ecb010e795de2065b64b032ef785623c0e32b
kucheriavii <kucheriavii@gmail.com>
2023-09-25 21:17:46 +0300

    317 - Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,19 @@ const Nav = () => {
         <Link to="/">
           <img src={logo} alt="comfy sloth" />
         </Link>
+        <button type="submit" className='nav-toggle'>
+          <FaBars />
+        </button>
       </div>
+      <ul className='nav-links'>
+        {links.map((link) => {
+          const {id, text, url} = link;
+          return <li key={id}>
+            <Link to={url}>{text}</Link>
+          </li>
+        })}
+      </ul>
+      <CartButtons />
     </div>
   </NavContainer>
 }
