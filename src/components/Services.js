05c9da108e846a461fc4a2e7e2f38b65a965e5a2
kucheriavii <kucheriavii@gmail.com>
2023-09-30 17:15:11 +0300

    327 - Home Page Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,25 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return <Wrapper>
+    <div className='section-center'>
+      <article className='header'>
+        <h3>custom furniture<br />
+        built only for you</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quo deleniti soluta doloremque, placeat fuga sunt cum, nemo voluptates reprehenderit nesciunt quibusdam corporis inventore cumque.</p>
+      </article>
+      <div className='services-center'>
+        {services.map((service) => {
+          const {id, icon, title, text} = service;
+          return <article key={id} className='service'>
+            <span className='icon'>{icon}</span>
+            <h4>{title}</h4>
+            <p>{text}</p>
+          </article>  
+      })}
+      </div>
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
